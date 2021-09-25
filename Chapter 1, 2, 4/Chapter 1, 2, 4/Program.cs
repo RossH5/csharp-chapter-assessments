2477d7e28d13ce7a50dea4a443513df90b7d969d
Ross Henke <rosshenke91@gmail.com>
2021-09-24 20:12:19 -0500

    Initial commit

---
+++ Chapter 1, 2, 4/Chapter 1, 2, 4/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Chapter_1__2__4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //##Chapter 1##
+            //1. n/a
+
+            //2.
+
+            //Represents the standard input, output, and error streams for console applications. This class cannot be inherited.
+
+            //3.
+
+            //Writes the text representation of the specified object, followed by the current line terminator, to the standard output
+            //stream using the specified format information.
+
+            //5.
+            Console.WriteLine("Good day")
+
+            //6.
+
+            Console.Write("First"");
+            Console.Write(" ");
+            Console.Write("Last");
+
+            //7. 
+
+            Console.WriteLine(1);
+            Console.WriteLine(101);
+            Console.WriteLine(1001);
+
+            //8.
+
+            Console.WriteLine(DateTime.Now);
+
+            //9.
+
+            Console.WriteLine(Math.Sqrt(12345));
+
+
+            //12.
+
+            //C# is a programming language developed by microsoft that runs on the .NET Framework. The .NET framework is the environment that compiles programs (C#, VB.NET, C++ and F#) to develop applications (console applications, Windows applications with a graphical user interface, web applications)
+
+
+            //##Chapter 2##
+            //8.
+
+            string hello = "Hello";
+            string world = "World";
+            object helloWorld = hello + " " + world;
+            string worldFinal = (string)helloWorld;
+
+            //12.
+
+            string firstName;
+            string lastName;
+            int age;
+            bool isManager;
+            int employeeNumber;
+
+            //##Chapter 4##
+            //6.
+
+            int a = 100;
+            int b = 99;
+            Console.WriteLine(Math.Max(a, b));
+
+            //7.
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
+            long sumOutput = a + b + c + d + e;
+            Console.WriteLine(sumOutput);
+        }
+    }
+}
---
+++ Chapter 3, 5/Chapter 3, 5/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace Chapter_3__5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //##Chapter 3##
+            //3.
+
+            Console.WriteLine("Input a Number:");
+            bool inputSuccess = int.TryParse(Console.ReadLine(), out int result);
+            int testResult = result / 100;
+            int finalResult = testResult % 10;
+            if (finalResult == 7 && inputSuccess == true)
+            {
+                Console.WriteLine("Equals 7");
+            }
+            else if (inputSuccess == false)
+            {
+                Console.WriteLine("input not an integer");
+            }
+            else
+            {
+                Console.WriteLine("Doesn't equal 7");
+            }
+
+            //7.
+
+            Console.WriteLine("Enter your weight");
+            bool inputSuccess = float.TryParse(Console.ReadLine(), out float result);
+            double moonWeight = result * .17;
+            moonWeight = Math.Round(moonWeight, 2);
+            if (inputSuccess == false)
+            {
+                Console.WriteLine("input not a number");
+            }
+            else
+            {
+                Console.WriteLine(moonWeight);
+            }
+
+            //##Chapter 5##
+            //2.
+
+            Console.WriteLine("Input 3 Numbers");
+            bool successOne = int.TryParse(Console.ReadLine(), out int numOne);
+            bool successTwo = int.TryParse(Console.ReadLine(), out int numTwo);
+            bool successThree = int.TryParse(Console.ReadLine(), out int numThree);
+            int negativeNumbersCount = 0;
+            if (successOne == false || successTwo == false || successThree == false)
+            {
+                Console.WriteLine("Atleast one Number is not valid");
+                Environment.Exit(0);
+            }
+            if (numOne < 0)
+            {
+                negativeNumbersCount++;
+            }
+            if (numTwo < 0)
+            {
+                negativeNumbersCount++;
+            }
+            if (numThree < 0)
+            {
+                negativeNumbersCount++;
+            }
+            if (numOne == 0 || numTwo == 0 || numThree == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if (negativeNumbersCount % 2 == 0)
+            {
+                Console.WriteLine("+");
+            }
+            else
+            {
+                Console.WriteLine("-");
+            }
+
+            //.5
+
+
+            Console.WriteLine("Input a number 1-9:");
+            int number = int.Parse(Console.ReadLine());
+            switch (number)
+            {
+                case 0:
+                    Console.WriteLine("Zero");
+                    break;
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+                case 4:
+                    Console.WriteLine("Four");
+                    break;
+                case 5:
+                    Console.WriteLine("Five");
+                    break;
+                case 6:
+                    Console.WriteLine("Six");
+                    break;
+                case 7:
+                    Console.WriteLine("Seven");
+                    break;
+                case 8:
+                    Console.WriteLine("Eight");
+                    break;
+                case 9:
+                    Console.WriteLine("Nine");
+                    break;
+                default:
+                    Console.WriteLine("Not a digit (0-9)");
+                    break;
+            }
+
+            //8.
+
+            Console.WriteLine("Input a number for a variable: 0 for Integer, 1 for Double, 2 for String");
+            int userInput = int.Parse(Console.ReadLine());
+            switch (userInput)
+            {
+                case 0:
+                    Console.WriteLine("Enter an Integer");
+                    int integerInput = int.Parse(Console.ReadLine());
+                    Console.WriteLine(++integerInput);
+                    break;
+                case 1:
+                    Console.WriteLine("Enter a double");
+                    double dblInput = double.Parse(Console.ReadLine());
+                    Console.WriteLine(++dblInput);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter a string");
+                    string stringInput = Console.ReadLine();
+                    Console.WriteLine(stringInput + "*");
+                    break;
+                default:
+                    Console.WriteLine("Not a valid input");
+                    break;
+            }
+    }
+}
