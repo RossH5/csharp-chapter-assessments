b7fe35bc6e62ead48c10aa7261ee8b22b167db70
Ross Henke <rosshenke91@gmail.com>
2021-11-04 22:25:48 -0500

    1st changes

--- MeetingApp/MeetingApp/Program.cs
+++ MeetingApp/MeetingApp/Program.cs
@@ -72,13 +72,14 @@ namespace MeetingApp
             do
             {
                 try
-                { 
-                   do
-                   { 
+                {
+                    do
+                    {
                         Console.WriteLine("Welcome to the Meeting App!\n");
                         Console.WriteLine("What would you like to do?\n1. Load calendar from file\n" +
                                 "2. Create new calendar\n0. Exit\n");
                         int welcomeSelection = int.Parse(Console.ReadLine());
+                        Console.WriteLine("\n");
                         switch (welcomeSelection)
                         {
                             case 1:
@@ -91,129 +92,158 @@ namespace MeetingApp
                             case 2:
                                 Console.Write("Where would you like to store the file? ");
                                 fileLocation = Console.ReadLine();
-                                File.Create(@fileLocation).Close();
+                                File.Create(fileLocation).Close();  
                                 welcomeMenu = false;
                                 Console.WriteLine("\n");
                                 break;
                             case 0:
-                                string fileString = "";
-
-                                if (fileLocation != "")
-                                {
-                                    foreach (Meeting myMeetings in listOfMeetings)
-                                    {
-                                        fileString += myMeetings.Title + "," + myMeetings.Location + "," + myMeetings.StartDateTime
-                                            + "," + myMeetings.EndDateTime + "\n";
-                                    }
-                                    File.WriteAllText(@fileLocation, fileString);
-                                }
-
                                 Environment.Exit(1);
                                 break;
                             default:
                                 Console.WriteLine("Invalid Input\n");
                                 break;
                         }
-                   } while (welcomeMenu);
+                    } while (welcomeMenu);
 
-                   do
-                   {
-                       Console.WriteLine("What would you like to do?\n\n1. Add meeting to calendar\n" +
-                            "2. Remove meeting from calendar\n3. View schedule\n0. Go Back\n");
-                       int userChoice = int.Parse(Console.ReadLine());
+                    do
+                    {
+                        Console.WriteLine("What would you like to do?\n\n1. Add meeting to calendar\n" +
+                             "2. Remove meeting from calendar\n3. View schedule\n0. Go Back\n");
+                        int userChoice = int.Parse(Console.ReadLine());
+                        Console.WriteLine("\n");
 
-                       switch (userChoice)
-                       {
-                           case 1:
-                               listOfMeetings.Add(AddMeeting());
-                               break;
-                           case 2:
-                               listOfMeetings = RemoveMeeting(listOfMeetings);
-                               break;
-                           case 3:
-                               DateTime start = new DateTime(DateTime.Today.Year, DateTime.Today.Month,
-                                        DateTime.Today.Day, 08, 00, 00, DateTimeKind.Local);
-                               DateTime end = new DateTime(DateTime.Today.Year, DateTime.Today.Month,
-                                    DateTime.Today.Day, 17, 00, 00, DateTimeKind.Local);
+                        switch (userChoice)
+                        {
+                            case 1:
+                                listOfMeetings = AddMeeting(listOfMeetings);
+                                break;
+                            case 2:
+                                listOfMeetings = RemoveMeeting(listOfMeetings);
+                                break;
+                            case 3:
+                                DateTime start = new DateTime(DateTime.Today.Year, DateTime.Today.Month,
+                                         DateTime.Today.Day, 08, 00, 00, DateTimeKind.Local);
+                                DateTime end = new DateTime(DateTime.Today.Year, DateTime.Today.Month,
+                                     DateTime.Today.Day, 17, 00, 00, DateTimeKind.Local);
 
-                               while (start != end)
-                               {
-                                   string meetingTitle = " ";
+                                while (start != end)
+                                {
+                                    string meetingTitle = " ";
 
-                                   foreach (Meeting meeting in listOfMeetings)
-                                   {
-                                       if ((start >= meeting.StartDateTime) & (start <= meeting.EndDateTime))
-                                       {
+                                    foreach (Meeting meeting in listOfMeetings)
+                                    {
+                                        if ((start >= meeting.StartDateTime) & (start <= meeting.EndDateTime))
+                                        {
                                             meetingTitle = meeting.Title;
-                                       }
-                                   }
-                                   Console.WriteLine("{0} | {1}", start.ToString("MM/dd/yy HH:mm"), meetingTitle);
+                                        }
+                                    }
+                                    Console.WriteLine("{0} | {1}", start.ToString("MM/dd/yy HH:mm"), meetingTitle);
 
-                                   start = start.AddMinutes(30);
-                               };
+                                    start = start.AddMinutes(30);
+                                };
 
-                               Console.WriteLine("\n");
-                               break;
-                           case 0:
-                               welcomeMenu = true;
-                               userMenu = false;
-                               break;
-                           default:
-                               Console.WriteLine("Invalid Input Try Again\n");
-                               break;
-                       }
-                   } while (userMenu);
+                                Console.WriteLine("\n");
+                                break;
+                            case 0:
+                                welcomeMenu = true;
+                                userMenu = false;
+                                break;
+                            default:
+                                Console.WriteLine("\nInvalid Input Try Again\n");
+                                break;
+                        }
+                    } while (userMenu);
+
+                    string fileString = "";
+
+                    if (fileLocation != "")
+                    {
+                        foreach (Meeting myMeetings in listOfMeetings)
+                        {
+                            fileString += myMeetings.Title + "," + myMeetings.Location + "," + myMeetings.StartDateTime
+                                 + "," + myMeetings.EndDateTime + "\n";
+                        }
+                        File.WriteAllText(@fileLocation, fileString);
+                    }
+                    fileLocation = "";
+
                 }
                 catch (FormatException)
                 {
-                    Console.WriteLine("Invalid Input try again\n");
+                    Console.WriteLine("\nInvalid Input try again\n");
+                    welcomeMenu = false;
                 }
                 catch (FileNotFoundException)
                 {
-                    Console.WriteLine("File Not Found \n");
-                    welcomeMenu = true;
-                }
-                catch (DirectoryNotFoundException)
-                {
-                    Console.WriteLine("Directory Not Found \n");
+                    Console.WriteLine("\nFile Not Found \n");
                     welcomeMenu = true;
                 }
                 catch (IOException)
                 {
-                    Console.WriteLine("File Location not valid. Please try again\n");
+                    Console.WriteLine("\nFile Location not valid. Please try again\n");
+                    fileLocation = "";
                     welcomeMenu = true;
                 }
             } while (true);
         }
 
-        static Meeting AddMeeting()
+        static List<Meeting> AddMeeting(List<Meeting> inputList)
         {
             var newMeeting = new Meeting();
-            bool addingMeeting = true;
-
-            do
+            try
             {
-                try
+                Console.Write("Meeting Title: ");
+                newMeeting.Title = Console.ReadLine();
+
+                Console.Write("Meeting Location: ");
+                newMeeting.Location = Console.ReadLine();
+
+                Console.Write("Meeting start date: ");
+                newMeeting.StartDateTime = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Meeting end date: ");
+                newMeeting.EndDateTime = DateTime.Parse(Console.ReadLine());
+
+                foreach (Meeting existingMeeting in inputList)
                 {
-                    Console.Write("Meeting Title: ");
-                    newMeeting.Title = Console.ReadLine();
-                    Console.Write("Meeting Location: ");
-                    newMeeting.Location = Console.ReadLine();
-                    Console.Write("Meeting start date: ");
-                    newMeeting.StartDateTime = DateTime.Parse(Console.ReadLine());
-                    Console.Write("Meeting end date: ");
-                    newMeeting.EndDateTime = DateTime.Parse(Console.ReadLine());
-                    return newMeeting;
-
+                    if ((newMeeting.StartDateTime <= existingMeeting.StartDateTime && newMeeting.EndDateTime >= existingMeeting.EndDateTime)
+                        || (newMeeting.StartDateTime >= existingMeeting.StartDateTime && newMeeting.StartDateTime < existingMeeting.EndDateTime)
+                         || (newMeeting.EndDateTime > existingMeeting.StartDateTime && newMeeting.EndDateTime <= existingMeeting.EndDateTime))
+                    {
+                        Console.Write("Time conflict with {0} {1} - {2}. Would you like to replace the prior meeting? (Y/N): ",
+                        existingMeeting.Title, existingMeeting.StartDateTime.ToString("f"), existingMeeting.EndDateTime.ToString("t"));
+                        string userInput = Console.ReadLine();
+                        switch (userInput)
+                        {
+                            case "YES":
+                            case "Yes":
+                            case "yes":
+                            case "Y":
+                            case "y":
+                                inputList.Remove(existingMeeting);
+                                inputList.Add(newMeeting);
+                                return inputList;
+                            case "NO":
+                            case "No":
+                            case "no":
+                            case "N":
+                            case "n":
+                                return inputList;
+                            default:
+                                Console.WriteLine("\nInput not recognized. Returning to Menu \n");
+                                return inputList;
+                        }
+                    }
                 }
-                catch
-                {
-                    Console.WriteLine("Meeting unable to be added\n");
-                }
-
-
-            } while (addingMeeting);
-            return newMeeting;
+                inputList.Add(newMeeting);
+                Console.WriteLine("\nMeeting Added!\n");
+                return inputList;
+            }
+            catch
+            {
+                Console.WriteLine("Meeting unable to be added");
+                return inputList;
+            }
         }
         static List<Meeting> RemoveMeeting(List<Meeting> inputList)
         {
@@ -230,9 +260,10 @@ namespace MeetingApp
 
             if (totalRemoved == 0)
             {
-                Console.WriteLine("Meeting not found \n");
+                Console.WriteLine("\nMeeting not found \n");
                 return inputList;
             }
+            Console.WriteLine("\nMeeting successfully removed!\n");
             return inputList;
         }
         static List<Meeting> LoadCalendar(string filePath)
@@ -240,7 +271,7 @@ namespace MeetingApp
             IEnumerable<string> fileContents = File.ReadLines(filePath);
             List<Meeting> returnList = new List<Meeting>();
 
-            foreach(string fileLine in fileContents)
+            foreach (string fileLine in fileContents)
             {
                 var newMeeting = new Meeting();
                 string[] fileLineArray = fileLine.Split(",");
@@ -252,9 +283,7 @@ namespace MeetingApp
             }
             return returnList;
         }
-
-
-    } 
+    }
 }
 
 
